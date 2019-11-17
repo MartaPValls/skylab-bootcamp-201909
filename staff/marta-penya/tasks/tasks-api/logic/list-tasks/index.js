73255c7ef54fe06da5c5587a660ddea803b5bf72
martapvalls <marpeva14@hotmail.com>
2019-11-17 13:05:59 +0100

    list and modify task with mongo

--- staff/marta-penya/tasks/tasks-api/logic/list-tasks/index.js
+++ staff/marta-penya/tasks/tasks-api/logic/list-tasks/index.js
@@ -1,21 +1,46 @@
 const validate = require('../../utils/validate')
-const users = require('../../data/users')()
-const tasks = require('../../data/tasks')()
 const { NotFoundError } = require('../../utils/errors')
+const database = require('../../utils/database')
+const { ObjectId } = database
 
 module.exports = function (id) {
     validate.string(id)
     validate.string.notVoid('id', id)
 
-    return new Promise((resolve, reject) => {
-        const user = users.data.find(user => user.id === id)
+    const client = database()
 
-        if (!user) return reject(new NotFoundError(`user with id ${id} not found`))
+    return client.connect()
+        .then(connection => {
+            const db = connection.db()
 
-        const _tasks = tasks.data.filter(({ user }) => user === id)
+            users = db.collection('users')
+            tasks = db.collection('tasks')
 
-        _tasks.forEach(task => task.lastAccess = new Date)
+            return users.findOne({ _id: ObjectId(id) })
+                .then(user => {
+                    if(!user) throw new NotFoundError(`user with id ${id} not found`)
 
-        tasks.persist().then(() => resolve(_tasks)).catch(reject)
-    })
+                    return tasks.updateMany({ user: ObjectId(id) }, { $set: { lastAccess: new Date }})
+                        .then(result => {
+                            if(!result.modifiedCount) throw Error('could not update tasks')
+
+                            return tasks.find({ user: ObjectId(id) }).toArray()
+                                .then(tasks => tasks)
+                        })
+                })
+        })
 }
+
+
+
+// return new Promise((resolve, reject) => {
+//     const user = users.data.find(user => user.id === id)
+
+//     if (!user) return reject(new NotFoundError(`user with id ${id} not found`))
+
+//     const _tasks = tasks.data.filter(({ user }) => user === id)
+
+//     _tasks.forEach(task => task.lastAccess = new Date)
+
+//     tasks.persist().then(() => resolve(_tasks)).catch(reject)
+// })
--- staff/marta-penya/tasks/tasks-api/logic/modify-task/index.js
+++ staff/marta-penya/tasks/tasks-api/logic/modify-task/index.js
@@ -1,7 +1,7 @@
 const validate = require('../../utils/validate')
-const users = require('../../data/users')()
-const tasks = require('../../data/tasks')()
 const { NotFoundError, ConflictError } = require('../../utils/errors')
+const database = require('../../utils/database')
+const { ObjectId } = database
 
 module.exports = function (id, taskId, title, description, status) {
     validate.string(id)
@@ -22,22 +22,52 @@ module.exports = function (id, taskId, title, description, status) {
         validate.matches('status', status, 'TODO', 'DOING', 'REVIEW', 'DONE')
     }
 
-    return new Promise((resolve, reject) => {
-        const user = users.data.find(user => user.id === id)
+    const client = database()
 
-        if (!user) return reject(new NotFoundError(`user with id ${id} not found`))
+    return client.connect()
+        .then(connection => {
+            const db = connection.db()
 
-        const task = tasks.data.find(({ id }) => id === taskId)
+            users = db.collection('users')
+            tasks = db.collection('tasks')
 
-        if (!task) return reject(new NotFoundError(`user does not have task with id ${taskId}`))
+            return users.findOne({ _id: ObjectId(id) })
+                .then(user => {
+                    if(!user) throw new NotFoundError(`user with id ${id} not found`)
 
-        if (task.user !== id) return reject(new ConflictError(`user with id ${id} does not correspond to task with id ${taskId}`))
+                    return tasks.findOne({ _id: ObjectId(taskId), user: ObjectId(id)})
+                        .then(task => {
+                            if(!task) return new NotFoundError(`user does not have task with id ${taskId}`)
 
-        title && (task.title = title)
-        description && (task.description = description)
-        status && (task.status = status)
-        task.lastAccess = new Date
+                            title && (task.title = title)
+                            description && (task.description = description)
+                            status && (task.status = status)
+                            task.lastAccess = new Date
 
-        tasks.persist().then(resolve).catch(reject)
-    })
+                            return tasks.updateOne({_id: ObjectId(taskId)}, { $set: { title: task.title, description: task.description, status: task.status, lastAccess: task.lastAccess }})
+                                .then(result => {
+                                    if(!result.modifiedCount) throw Error('could not modify tasks')
+                                })
+                        })
+                })
+})
 }
+
+// return new Promise((resolve, reject) => {
+//     const user = users.data.find(user => user.id === id)
+
+//     if (!user) return reject(new NotFoundError(`user with id ${id} not found`))
+
+//     const task = tasks.data.find(({ id }) => id === taskId)
+
+//     if (!task) return reject(new NotFoundError(`user does not have task with id ${taskId}`))
+
+//     if (task.user !== id) return reject(new ConflictError(`user with id ${id} does not correspond to task with id ${taskId}`))
+
+//     title && (task.title = title)
+//     description && (task.description = description)
+//     status && (task.status = status)
+//     task.lastAccess = new Date
+
+//     tasks.persist().then(resolve).catch(reject)
+// })

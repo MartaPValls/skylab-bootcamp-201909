94f786c1bd07720014964b9c0b2c153654352747
martapvalls <marpeva14@hotmail.com>
2019-11-18 14:45:36 +0100

    remove task into mongo

--- staff/marta-penya/tasks/tasks-api/logic/remove-task/index.js
+++ staff/marta-penya/tasks/tasks-api/logic/remove-task/index.js
@@ -1,3 +1,28 @@
+const validate = require('../../utils/validate')
+const { NotFoundError } = require('../../utils/errors')
+const database = require('../../utils/database')
+const { ObjectId } = database
+
 module.exports = function(id, taskId) {
-    // TODO
+    validate.string(id)
+    validate.string.notVoid('id', id)
+    validate.string(taskId)
+    validate.string.notVoid('taskId', taskId)
+
+    const client = database()
+
+    return client.connect()
+        .then(connection => {
+            const db = connection.db()
+
+            users = db.collection('users')
+            tasks = db.collection('tasks')
+
+            return users.findOne({ _id: ObjectId(id) })
+                .then(user => {
+                    if(!user) throw new NotFoundError(`user with id ${id} not found`)
+
+                    return tasks.deleteOne({ _id: ObjectId(taskId) })
+                })
+        })
 }
